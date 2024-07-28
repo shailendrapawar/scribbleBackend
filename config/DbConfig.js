e5bf1704e4e9af03d738bc57fc51f41d40703938
shailendrapawar <shailendrapawar980@gmail.com>
2024-07-28 23:44:06 +0530

    fee

--- config/DbConfig.js
+++ config/DbConfig.js
@@ -1,8 +1,14 @@
 const mongoose=require("mongoose")
 require("dotenv").config()
+
 const DbConnect=async()=>{
    let dbRes= await mongoose.connect(process.env.MONGO_URL)
-   dbRes?console.log("db connected"):console.log("db connection issue")
+   if(dbRes){
+      console.log("db connected")
+   }else{
+      console.log("db connection issue"+dbRes.err)
+   }
+   // process.env.MONGO_URL
 
 }
 
